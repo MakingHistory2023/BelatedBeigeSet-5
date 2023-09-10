a6155ef630e55d130feee87e9723e4883950e9e2
MakingHistory2023 <143676019+MakingHistory2023@users.noreply.github.com>
2023-09-10 02:09:38 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Oh-oh-oh-oh-oh,");
+    Console.WriteLine ("oh-oh-oh-oh,");
+    Console.WriteLine ("oh-oh-oh/");
+    Console.WriteLine ("Caught in a bad romance...");
+  }
+}
